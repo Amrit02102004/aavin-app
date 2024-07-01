002a77fe5edc5a26a798b7c6e42e7a02d4061eb3
Vijay Varadarajan <vijayvaradarajan84@gmail.com>
2024-07-01 23:59:49 +0530

    temp fixes commented lotta code

--- src/Pages/Productpage.jsx
+++ src/Pages/Productpage.jsx
@@ -1,38 +1,38 @@
 import React, { useState, useEffect } from 'react'
 import ItemCard from '../Components/ItemCard'
 import SidebarComponent from '../Components/SideBar'
-import { getAllProducts, addToCart } from '../drizzle/api'
+// import { getAllProducts, addToCart } from '../drizzle/api'
 
 const Productpage = () => {
-    const [products, setProducts] = useState([]);
-    const [cartItems, setCartItems] = useState([]);
+    // const [products, setProducts] = useState([]);
+    // const [cartItems, setCartItems] = useState([]);
 
-    useEffect(() => {
-        fetchProducts();
-    }, []);
+    // useEffect(() => {
+    //     fetchProducts();
+    // }, []);
 
-    const fetchProducts = async () => {
-        try {
-            const fetchedProducts = await getAllProducts();
-            setProducts(fetchedProducts);
-        } catch (error) {
-            console.error('Error fetching products:', error);
-        }
-    };
+    // const fetchProducts = async () => {
+    //     try {
+    //         const fetchedProducts = await getAllProducts();
+    //         setProducts(fetchedProducts);
+    //     } catch (error) {
+    //         console.error('Error fetching products:', error);
+    //     }
+    // };
 
-    const handleAddToCart = async (productId, quantity) => {
-        try {
-            await addToCart(1, productId, quantity); // Assuming user ID is 1
-            setCartItems([...cartItems, productId]);
-        } catch (error) {
-            console.error('Error adding to cart:', error);
-        }
-    };
+    // const handleAddToCart = async (productId, quantity) => {
+    //     try {
+    //         await addToCart(1, productId, quantity); // Assuming user ID is 1
+    //         setCartItems([...cartItems, productId]);
+    //     } catch (error) {
+    //         console.error('Error adding to cart:', error);
+    //     }
+    // };
 
     return (
         <>
             <SidebarComponent>
-                <div className="text-center">
+                {/* <div className="text-center">
                     <h1 className='mt-2'>Products page</h1>
                 </div>
                 <div className='d-flex flex-wrap'>
@@ -45,7 +45,7 @@ const Productpage = () => {
                             onAddToCart={(quantity) => handleAddToCart(product.id, quantity)}
                         />
                     ))}
-                </div>
+                </div> */}
             </SidebarComponent>
         </>
     )
